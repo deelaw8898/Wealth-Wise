d87d0757dfbd2ce005fe3fd925b8695ec375ef7b
Preston Peters (ptp726) <ptp726@mail.usask.ca>
2023-10-15 19:15:39 -0600

    Revert "Added app.css and app.js files for the car payment calculator"
    This reverts commit 5ab0fbfa947e03a002e49b0275dfef9480467369

---
+++ Frontend/src/pages/Car_payment/car_pay.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function car(){
+    return(
+        <section>
+            <div class="container-fluid">
+                <h1 class ="mt-5">
+                    In development 
+                </h1>
+                <p>
+                    this the car payment calculator 
+                </p>
+
+            </div>
+        </section>
+    );
+}
+
+export default car;
