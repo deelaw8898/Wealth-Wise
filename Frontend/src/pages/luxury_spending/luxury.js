f62523b8b7c51a03dddde59d7049eef588604bd2
David Simonov <davidsimonov324@gmail.com>
2023-11-05 13:12:26 -0600

    savings per month feature works

--- Frontend/src/pages/luxury_spending/luxury.js
+++ Frontend/src/pages/luxury_spending/luxury.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
-function luxury(){
-    return(
-        <section>
-            <div class="container-fluid">
-                <h1 class ="mt-5">
-                    In development 
-                </h1>
-                <p>
-                    this the Luxury payment calculator
-                </p>
+function Luxury() {
+  const [itemPrice, setItemPrice] = useState("");
+  const [timePeriod, setTimePeriod] = useState("");
+  const [totalSpending, setTotalSpending] = useState(0);
+  const [monthlySavings, setMonthlySavings] = useState(0);
 
-            </div>
-        </section>
-    );
+  const calculateTotalSpending = () => {
+    const taxRate = 0.1; // 10% tax rate
+    const itemPriceNumber = parseFloat(itemPrice);
+    const timePeriodNumber = parseInt(timePeriod, 10);
+
+    if (!isNaN(itemPriceNumber) && !isNaN(timePeriodNumber)) {
+      const taxAmount = itemPriceNumber * taxRate;
+      const totalPrice = itemPriceNumber + taxAmount;
+      setTotalSpending(totalPrice);
+
+      const requiredSavings = totalPrice / timePeriodNumber;
+      setMonthlySavings(requiredSavings.toFixed(2));
+    }
+  };
+
+  return (
+    <section>
+      <div className="container-fluid">
+        <h1 className="mt-5">Luxury Spending Calculator</h1>
+        <div className="input-group mb-3">
+          <input
+            type="number"
+            className="form-control"
+            placeholder="Item Price"
+            value={itemPrice}
+            onChange={(e) => setItemPrice(e.target.value)}
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <input
+            type="number"
+            className="form-control"
+            placeholder="Time Period (in months)"
+            value={timePeriod}
+            onChange={(e) => setTimePeriod(e.target.value)}
+          />
+        </div>
+
+        {totalSpending > 0 && (
+          <div>
+            <p>Total Spending (including tax): ${totalSpending.toFixed(2)}</p>
+            <p>Monthly Savings Required: ${monthlySavings}</p>
+          </div>
+        )}
+      </div>
+      <button className="btn btn-primary" onClick={calculateTotalSpending}>
+        Calculate
+      </button>
+    </section>
+  );
 }
 
-export default luxury;
+export default Luxury;
