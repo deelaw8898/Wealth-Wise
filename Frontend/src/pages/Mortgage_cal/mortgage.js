3600cefe66f4e03784e0d25e4e63746f185e3a82
karankairon <bsk610@usask.ca>
2023-11-17 19:35:31 -0600

    added regression testing to mortCal and updated CarPay calc

--- Frontend/src/pages/Mortgage_cal/mortgage.js
+++ Frontend/src/pages/Mortgage_cal/mortgage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Pie } from 'react-chartjs-2';
 import './mortgage.css';
 
@@ -67,6 +67,21 @@ function MortgageCalculator() {
   };
 
   const calculateMortgage = () => {
+
+    // error checks
+    if (!homePrice) {
+      alert('You left the Home Price field empty!');
+      return;
+    }
+    // if (!downPayment) {
+    //   alert('You left the Down Payment field empty!');
+    //   return;
+    // }
+    if (!interestRate) {
+      alert('You left the Interest Rate field empty!');
+      return;
+    }
+
     const principal = homePrice - downPayment;
     const monthlyRate = interestRate / 1200;
     const numberOfPayments = loanTerm * 12;
@@ -88,6 +103,87 @@ function MortgageCalculator() {
       ],
     });
   };
+
+  // cloned CalculateMortgage function for testing purposes
+  const mockCalculateMortgage = (homePrice, downPayment, interestRate, loanTerm, province) => {
+    const principal = homePrice - downPayment;
+    const monthlyRate = interestRate / 1200;
+    const numberOfPayments = loanTerm * 12;
+    const taxRate = PROVINCES_TAX_RATES[province] / 100;
+    const monthlyTax = (homePrice * taxRate) / 12;
+
+    let monthlyPrincipalAndInterest = principal * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -numberOfPayments)));
+    let totalMonthlyPayment = monthlyPrincipalAndInterest + monthlyTax;
+
+    return {
+      totalMonthlyPayment: parseFloat(totalMonthlyPayment.toFixed(2))
+    };
+  };
+
+  // function for running regression test
+  const runRegressionTests = () => {
+    const testCases = [
+      {
+        id: '1',
+        description: 'Standard 10-year mortgage in Alberta',
+        data: { homePrice: 300000, downPayment: 60000, interestRate: 3.5, loanTerm: 10, province: 'Alberta' },
+        expected: { totalMonthlyPayment: 2523.26 } 
+      },
+      {
+        id: '2',
+        description: '15-year mortgage in Ontario',
+        data: { homePrice: 500000, downPayment: 100000, interestRate: 4.0, loanTerm: 15, province: 'Ontario' },
+        expected: { totalMonthlyPayment: 3271.25 } 
+      },
+      {
+        id: '3',
+        description: '30-year mortgage in Nova Scotia',
+        data: { homePrice: 200000, downPayment: 20000, interestRate: 3.0, loanTerm: 30, province: 'NovaScotia' },
+        expected: { totalMonthlyPayment: 925.55 } 
+      },
+      {
+        id: '4',
+        description: '15-year mortgage in Quebec with no down payment',
+        data: { homePrice: 350000, downPayment: 0, interestRate: 3.75, loanTerm: 15, province: 'Quebec' },
+        expected: { totalMonthlyPayment: 2793.20 } 
+      },
+      {
+        id: '5',
+        description: '30-year mortgage in British Columbia with a large down payment',
+        data: { homePrice: 600000, downPayment: 300000, interestRate: 4.25, loanTerm: 30, province: 'BritishColumbia' },
+        expected: { totalMonthlyPayment: 1825.82 } 
+      },
+      {
+        id: '6',
+        description: '10-year mortgage in Manitoba',
+        data: { homePrice: 400000, downPayment: 80000, interestRate: 3.25, loanTerm: 10, province: 'Manitoba' },
+        expected: { totalMonthlyPayment: 3527.01 } 
+      }
+    ];
+
+    testCases.forEach(testCase => {
+      console.log(`Running Test Case: ${testCase.id} - ${testCase.description}`);
+
+      const result = mockCalculateMortgage(
+        testCase.data.homePrice,
+        testCase.data.downPayment,
+        testCase.data.interestRate,
+        testCase.data.loanTerm,
+        testCase.data.province
+      );
+
+      if (result.totalMonthlyPayment !== testCase.expected.totalMonthlyPayment) {
+        console.error(`Test Case ${testCase.id} Failed. Expected ${testCase.expected.totalMonthlyPayment}, but got ${result.totalMonthlyPayment}`);
+      } else {
+        console.log(`Test Case ${testCase.id} Passed.`);
+      }
+    });
+  };
+
+  useEffect(() => {
+    runRegressionTests();
+  }, []);
+
 
   return (
     <div className='page-scroller'>
