7256603bde633ee31958740fb3b61fe1e58e355d
Preston Peters <ptp726@usask.ca>
2023-11-22 19:13:25 -0600

    Fixing navbar or at least attempting to.

--- Frontend/src/pages/Nav/nav.js
+++ Frontend/src/pages/Nav/nav.js
@@ -4,25 +4,25 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
     return (
-        <nav className="navbar">
+        <nav className="navigation">
             {/* Top row for brand and auth links */}
             <div className="top-row">
-                <span className="navbar-brand">WealthWise</span>
+                <span className="navigation-brand">WealthWise</span>
                 <div className="auth-links">
-                    <Link to='/login' className="nav-item nav-link auth">Login</Link>
-                    <Link to='/registration' className="nav-item nav-link auth special">Join us</Link>
+                    <Link to='/login' className="navigation-item navigation-link auth">Login</Link>
+                    <Link to='/registration' className="navigation-item navigation-link auth special">Join us</Link>
                 </div>
             </div>
 
             {/* Bottom row for navigation buttons */}
-            <div className="navbar-nav">
-                <Link to='/' className="nav-item nav-link">Home</Link>
-                <Link to='/mortgage' className="nav-item nav-link">Mortgage Calculator</Link>
-                <Link to='/car' className="nav-item nav-link">Car Payment</Link>
-                <Link to='/debt' className="nav-item nav-link">Debt Calculator</Link>
-                <Link to='/luxury' className="nav-item nav-link">Luxury Spending</Link>
-                <Link to='/vacation' className="nav-item nav-link">Vacation Spending</Link>
-                <Link to='/tracker' className="nav-item nav-link">Spending</Link>
+            <div className="navigation-options">
+                <Link to='/' className="navigation-item navigation-link">Home</Link>
+                <Link to='/mortgage' className="navigation-item navigation-link">Mortgage Payment</Link>
+                <Link to='/car' className="navigation-item navigation-link">Car Payment</Link>
+                <Link to='/debt' className="navigation-item navigation-link">Debt Repayment</Link>
+                <Link to='/luxury' className="navigation-item navigation-link">Luxury Spending</Link>
+                <Link to='/vacation' className="navigation-item navigation-link">Vacation Spending</Link>
+                <Link to='/tracker' className="navigation-item navigation-link">Spending Tracker</Link>
             </div>
         </nav>
     );
